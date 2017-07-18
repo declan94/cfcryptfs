1f1ce3009ba805aea0e9a130e688edfd6c9c18f6
陈晔 <chenye94@qq.com>
2017-07-18 16:56:16 -0700

    keycrypter docs

--- keycrypter/doc.go
+++ keycrypter/doc.go
@@ -1,9 +1,11 @@
 /*
 Package keycrypter provides two ways to safely store secret encryption keys.
-	1) Password protection.
-		Ask user for password and encrypt the key using the password before storing.
-	2) Shamir's Secret Sharing
-		Use Shamir's Secret Sharing algorithm to split the key into several parts and store in different places(media).
-		All or a threshold count of the split keys are needed for reconstruct the key.
+
+1) Password protection.
+Ask user for password and encrypt the key using the password before storing.
+
+2) Shamir's Secret Sharing
+Use Shamir's Secret Sharing algorithm to split the key into several parts and store in different places(media).
+All or a threshold count of the split keys are needed for reconstruct the key.
 */
 package keycrypter
